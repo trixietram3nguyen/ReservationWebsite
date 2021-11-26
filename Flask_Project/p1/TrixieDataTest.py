c8e54e8db5dc31489c1fb3fc604cde695830383a
Trixie Tram Nguyen <57166775+trixietram3nguyen@users.noreply.github.com>
2021-11-26 16:06:59 -0600

    Update TrixieDataTest.py
    Logic to book reservations

--- Flask_Project/p1/TrixieDataTest.py
+++ Flask_Project/p1/TrixieDataTest.py
@@ -10,3 +10,27 @@ tables = db.tables
 # To use the customers collections
 customers = db.customers
 
+# Making a reservations
+# Need to generate random string with letters and numbers for the confirmation code
+import string
+import random
+
+# function to generate confirmation code
+def id_generator(size=6, chars=string.ascii_lowercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
+
+# info to book reservation
+confirmation = id_generator()
+
+# Find if the date picked has been reserved
+    # If yes -> check the time picked has been reserved
+        # If yes -> check what table(s) that reserved for chosen date and time
+            # If all tables reserved with the chosen time and date
+                # Promt choose different reservation time or date or both
+            # If not all tables reserved with the chosen time and date
+                # Book the reservation with available tables -> Insert the reservation into the database
+                # Reservation successfully booked, email the confirmation
+        # If no -> book the reservation -> insert the reservation into the data
+        # Reservation successfully booked, email the confirmation
+    # If no -> book the reservations -> insert the reservation into the database
+    # Reservation successfully booked, email the confirmation
