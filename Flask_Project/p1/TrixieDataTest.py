4d4b2424ea7bb7d5aa4ce205aa14278cb759421c
Trixie Tram Nguyen <57166775+trixietram3nguyen@users.noreply.github.com>
2021-11-26 17:23:34 -0600

    reservations logic

--- Flask_Project/p1/TrixieDataTest.py
+++ Flask_Project/p1/TrixieDataTest.py
@@ -20,10 +20,25 @@ def id_generator(size=6, chars=string.ascii_lowercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
 
 # info to book reservation
+table = []
+# print("THE FIRST TABLE LENGTH\n")
+# print(len(table))
 confirmation = id_generator()
+print("The confirmation code: " + confirmation)
 
 # Find if the date picked has been reserved
+query = tables.count({"book_status.date":"11/20/2021"})
+# print(query)
+if query != 0:
     # If yes -> check the time picked has been reserved
+    print("There are table(s) reserved with this date")
+    query = tables.find({"book_status.date":"11/20/2021"},{"table_number":1,"_id":0})
+    
+    for result in query:
+        table.append(result['table_number'])
+    
+    print(table)
+    print(len(table))
         # If yes -> check what table(s) that reserved for chosen date and time
             # If all tables reserved with the chosen time and date
                 # Promt choose different reservation time or date or both
@@ -32,5 +47,14 @@ confirmation = id_generator()
                 # Reservation successfully booked, email the confirmation
         # If no -> book the reservation -> insert the reservation into the data
         # Reservation successfully booked, email the confirmation
+else:
     # If no -> book the reservations -> insert the reservation into the database
+    print("This date can be reserve now")
+    # Check the table size before booking
+    # 5 cases to book
+    # case 1: if party size is 4 or less use table any table but all available so automatically use 3
+    # case 2: if party size is 5 or less use table 1/2/7/8 but all available so automatically use 2
+    # case 3: if party size is 7 or less use table 1/7 but both available so automatically use 1
+    # case 4: if party size is 8 or less use table 3+5/4+6 but both available so automatically use 3+5
+    # case 5: if party size is 12 or less use table 1+2/7+8 but both available so automatically use 1+2
     # Reservation successfully booked, email the confirmation
