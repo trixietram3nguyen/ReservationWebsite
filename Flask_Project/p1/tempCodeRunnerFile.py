215098502e2c86f5027155073c2474e0832b58bf
Trixie Tram Nguyen <57166775+trixietram3nguyen@users.noreply.github.com>
2021-12-01 04:04:42 -0600

    TrixieDataTest.py and reservations_queries.txt Updated
    Reservation system completed

--- Flask_Project/p1/tempCodeRunnerFile.py
+++ Flask_Project/p1/tempCodeRunnerFile.py
@@ -1,2 +1,20 @@
-print(table_num)
-        print(table_size)
+if (best_table[1] == 0):
+    #     # Yes -> Check if best_table[0] is 0
+    #     if (best_table[0] == 0):
+    #         # Yes -> No more table to reserve
+    #         print("Please pick another time/date.")
+    #     else:
+    #         # No -> There are table to reserve
+    #         # Call the addReservation function once
+    #         print("Add reservation for table " + str(best_table[0]))
+    #         # Reserving the table
+    #         addReservation(best_table[0], date, time, confirmation, name, party_size, email, best_table[1])
+    # else:
+    #     # No -> Table need to be combine
+    #     # Call the addReservation function twice, flip the table insert and table combine for the second call
+    #     print("Add reservation for table " + str(best_table[0]))
+    #     # Reserving the table
+    #     addReservation(best_table[0], date, time, confirmation, name, party_size, email, best_table[1])
+    #     # Reserving the table
+    #     print("Add reservation for table " + str(best_table[1]))
+    #     addReservation(best_table[1], date, time, confirmation, name, party_size, email, best_table[0])
