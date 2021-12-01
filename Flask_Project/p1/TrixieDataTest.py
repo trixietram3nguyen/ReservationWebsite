f8a5041eefe29e7157c4ab0f5fd9a9394dc402e8
Trixie Tram Nguyen <57166775+trixietram3nguyen@users.noreply.github.com>
2021-12-01 03:01:00 -0600

    Update TrixieDataTest.py

--- Flask_Project/p1/TrixieDataTest.py
+++ Flask_Project/p1/TrixieDataTest.py
@@ -121,8 +121,8 @@ party_size = 5
 # print("THE FIRST TABLE LENGTH\n")
 # print(len(table))
 confirmation = id_generator()
-time = ["8:00pm","8:15pm","8:30pm","8:45pm","8:00pm","8:15pm"]
-date = "11/29/2021"
+time = ["5:30pm","5:45pm","6:00pm","6:15pm"]
+date = "11/28/2021"
 name = "TT"
 email = "TW"
 combined_tb = 0
@@ -273,27 +273,27 @@ else:
     print("The table will be reserve is")
     print(best_table)
 
-    # # If best_table[1] is 0 -> table will need to be combine or non of the table can be reserve
-    # if (best_table[1] == 0):
-    #     # Yes -> Check if best_table[0] is 0
-    #     if (best_table[0] == 0):
-    #         # Yes -> No more table to reserve
-    #         print("Please pick another time/date.")
-    #     else:
-    #         # No -> There are table to reserve
-    #         # Call the addReservation function once
-    #         print("Add reservation for table " + str(best_table[0]))
-    #         # Reserving the table
-    #         addReservation(best_table[0], date, time, confirmation, name, party_size, email, best_table[1])
-    # else:
-    #     # No -> Table need to be combine
-    #     # Call the addReservation function twice, flip the table insert and table combine for the second call
-    #     print("Add reservation for table " + str(best_table[0]))
-    #     # Reserving the table
-    #     addReservation(best_table[0], date, time, confirmation, name, party_size, email, best_table[1])
-    #     # Reserving the table
-    #     print("Add reservation for table " + str(best_table[1]))
-    #     addReservation(best_table[1], date, time, confirmation, name, party_size, email, best_table[0])
+    # If best_table[1] is 0 -> table will need to be combine or non of the table can be reserve
+    if (best_table[1] == 0):
+        # Yes -> Check if best_table[0] is 0
+        if (best_table[0] == 0):
+            # Yes -> No more table to reserve
+            print("Please pick another time/date.")
+        else:
+            # No -> There are table to reserve
+            # Call the addReservation function once
+            print("Add reservation for table " + str(best_table[0]))
+            # Reserving the table
+            addReservation(best_table[0], date, time, confirmation, name, party_size, email, best_table[1])
+    else:
+        # No -> Table need to be combine
+        # Call the addReservation function twice, flip the table insert and table combine for the second call
+        print("Add reservation for table " + str(best_table[0]))
+        # Reserving the table
+        addReservation(best_table[0], date, time, confirmation, name, party_size, email, best_table[1])
+        # Reserving the table
+        print("Add reservation for table " + str(best_table[1]))
+        addReservation(best_table[1], date, time, confirmation, name, party_size, email, best_table[0])
 
 
 
