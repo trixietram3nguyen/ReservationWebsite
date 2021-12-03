ab37ff5b9ce5749ec220bf79613272ee5a7a19d7
Trixie Tram Nguyen <57166775+trixietram3nguyen@users.noreply.github.com>
2021-12-03 00:30:47 -0600

    Update TrixieDataTest.py

--- Flask_Project/p1/TrixieDataTest.py
+++ Flask_Project/p1/TrixieDataTest.py
@@ -10,4 +10,29 @@ tables = db.tables
 # To use the customers collections
 customers = db.customers
 
+# Getting information of the customer for the profile page
+email = "rtq.nguyen12@gmail.com"
 
+query = customers.find({"email":"rtq.nguyen12@gmail.com"})
+for result in query:
+    profile = result
+
+pf_firstName = profile['first_name']
+pf_lastName = profile['last_name']
+pf_email = profile['email']
+pf_pass = profile['pass']
+pf_mailingAdd = profile['mailing_address']
+pf_billingAdd = profile['billing_address']
+pf_card = profile['card']
+pf_points = profile['points']
+pf_diner = profile['preferred_diner']
+
+print(pf_firstName + "\n")
+print(pf_lastName + "\n")
+print(pf_email + "\n")
+print(pf_pass + "\n")
+print(pf_mailingAdd + "\n")
+print(pf_billingAdd + "\n")
+print(pf_card + "\n")
+print(pf_points + "\n")
+print(pf_diner + "\n")
